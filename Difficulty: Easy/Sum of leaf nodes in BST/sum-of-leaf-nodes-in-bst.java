31f71a51f59786c51aaa588097cc20113f7931b2
LLArmanKhan <useroneplus067@gmail.com>
2026-07-14 21:19:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of leaf nodes in BST/sum-of-leaf-nodes-in-bst.java
@@ -0,0 +1,19 @@
+/*node class of the binary ssearch tree
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int key)
+    {
+        data = key;
+        left = right = null;
+    }
+}*/
+class Solution {
+    public static int sumOfLeafNodes(Node root) {
+        // code here
+        if(root==null) return 0;
+        if(root.left==null && root.right==null) return root.data;
+        else return sumOfLeafNodes(root.left)+sumOfLeafNodes(root.right);
+    }
+}
