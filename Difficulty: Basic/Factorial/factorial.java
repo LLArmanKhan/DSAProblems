618ed33a8f5a52725dd2229d0c2ccc9fe512c8de
LLArmanKhan <useroneplus067@gmail.com>
2026-07-14 13:24:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -2,6 +2,10 @@ class Solution {
     int factorial(int n) {
         // code here
         if(n==0) return 1;
-        return n*factorial(n-1);
+        return revfact(1,n);
+    }
+    int revfact(int a,int n){
+        if(a==n) return n;
+        return a*revfact(a+1,n);
     }
 }
